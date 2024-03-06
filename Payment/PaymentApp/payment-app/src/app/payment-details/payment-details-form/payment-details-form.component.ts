8518b0086ce369e1d2279fbf845625286e17f477
Thanh Sang <anhsang31797@example.com>
2024-03-06 17:15:19 +0700

    create service

---
+++ Payment/PaymentApp/payment-app/src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'payment-app-payment-details-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment-details-form.component.html',
+  styleUrl: './payment-details-form.component.css',
+})
+export class PaymentDetailsFormComponent {}
---
+++ Payment/PaymentApp/payment-app/src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'payment-app-payment-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment-details.component.html',
+  styleUrl: './payment-details.component.css',
+})
+export class PaymentDetailsComponent {}
